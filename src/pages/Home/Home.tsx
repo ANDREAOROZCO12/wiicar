fa06403a5453ed4b49307bf2b4f66893347f4a38
ANDREAOROZCO12 <andre_orozco12@hotmail.com>
2022-12-05 10:09:38 -0500

    add timer loading

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -28,10 +28,10 @@ const Home = () => {
   const initialCont: Array<{ [key: string]: any }> = [];
   const swal = () => {
     Swal.fire({
-      title: "Cargando",
+      title: "",
       html: '<div class="swal"> <img src="https://acegif.com/wp-content/uploads/loading-12.gif" alt="" /> <div><p class="swalTitle">Cargue masivo</p><p>Espera un momento mientras procesamos los datos, esto puede tardar unos minutos</p></div></div>',
       showConfirmButton: false,
-      // timer: 3000,
+      timer: 3000,
     }).then(() => {
       window.location.replace("/carlist");
     });
